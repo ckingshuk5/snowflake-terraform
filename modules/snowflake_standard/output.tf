6e3d3191efa6db8465a4675984a8325cabc62c7c
ckingshuk5 <ckingshuk5@gmail.com>
2026-04-25 01:35:27 +0530

    hey ram2

--- modules/snowflake_standard/output.tf
+++ modules/snowflake_standard/output.tf
@@ -7,5 +7,12 @@ output "databases_created" {
   value = [snowflake_database.ibor_db.name, snowflake_database.ism_db.name,
     snowflake_database.data_product_db.name, snowflake_database.apple_src_db.name]
 }
+output "debug_account_n" {
+  value = var.account_name_n
+}
+
+output "debug_account_c" {
+  value = var.account_name_c
+}
 
 
--- output.tf
+++
@@ -1,7 +0,0 @@
-output "debug_account_n" {
-  value = var.account_name_n
-}
-
-output "debug_account_c" {
-  value = var.account_name_c
-}
